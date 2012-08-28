1c052a4a42dbca2b210d8392492f4297d6d1be1e
Chris Paul <cmpaul@gmail.com>
2012-08-27 23:01:25 -0700

    Add Apache license.

--- jmx-dashlet-repo-amp/src/main/java/com/tribloom/module/JmxClient.java
+++ jmx-dashlet-repo-amp/src/main/java/com/tribloom/module/JmxClient.java
@@ -17,6 +17,25 @@ import org.springframework.extensions.webscripts.DeclarativeWebScript;
 import org.springframework.extensions.webscripts.Status;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 
+/**
+ *    
+ *    Copyright 2012 Tribloom, Inc.
+ *    
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *    
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *    
+ * @author Chris Paul, Tribloom, Inc.
+ * 
+ */
 public class JmxClient extends DeclarativeWebScript {
 
 	// TODO: How to add a log4j logger?
